fbede0c317a43dac53a40f04446feded5568a7f7
K-jisu <wltn7star@naver.com>
2025-03-19 03:13:21 +0900

    fix : url

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -3,7 +3,7 @@ import RIOT_CONSTANT from "@/constants/RIOT_CONSTANT";
 export async function GET() {
   const res = await fetch(`${RIOT_CONSTANT.RIOT_ROTATION_CHAMPION_URL}`, {
     headers: {
-      "X-Riot-Token": `${process.env.NEXT_PUBLIC_RIOT_API_KEY}`,
+      "X-Riot-Token": `${process.env.NEXT_PUBLIC_RIOT_API_KEY}` || "",
     },
   });
 
