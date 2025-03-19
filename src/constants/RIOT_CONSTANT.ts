a61a4f62244ab1206df89d196c03c7cfc434b170
K-jisu <wltn7star@naver.com>
2025-03-19 09:32:01 +0900

    🐛 fix : 절대경로 추가

--- src/constants/RIOT_CONSTANT.ts
+++ src/constants/RIOT_CONSTANT.ts
@@ -2,13 +2,13 @@ const RIOT_URL = "https://ddragon.leagueoflegends.com";
 const RIOT_IMG_URL = "https://ddragon.leagueoflegends.com/cdn/15.5.1/img/";
 const RIOT_ROTATION_CHAMPION_URL =
   "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations";
-// const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://lol-app-dun.vercel.app/";
 
 const RIOT_CONSTANT = {
   RIOT_URL,
   RIOT_IMG_URL,
   RIOT_ROTATION_CHAMPION_URL,
-  // BASE_URL,
+  BASE_URL,
 };
 
 export default RIOT_CONSTANT;
