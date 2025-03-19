1710637834f170c027d60f3f84c28aef4431b869
K-jisu <wltn7star@naver.com>
2025-03-19 09:04:45 +0900

    fix : tryCatch

--- src/app/rotation/page.tsx
+++ src/app/rotation/page.tsx
@@ -3,14 +3,18 @@ import fetchRotationChampion from "@/utils/riotApi";
 import { fetchChampion } from "@/utils/serverApi";
 
 const serverPage = async () => {
-  const rotationChamps = await fetchRotationChampion();
-  const champs = await fetchChampion();
-  return (
-    <RotationClientPage
-      rotationInitial={rotationChamps}
-      champsInitial={champs}
-    />
-  );
+  try {
+    const rotationChamps = await fetchRotationChampion();
+    const champs = await fetchChampion();
+    return (
+      <RotationClientPage
+        rotationInitial={rotationChamps}
+        champsInitial={champs}
+      />
+    );
+  } catch (error) {
+    console.log("데이터를 불러오는데 실패했습니다.");
+  }
 };
 
 export default serverPage;
--- src/utils/riotApi.ts
+++ src/utils/riotApi.ts
@@ -1,12 +1,13 @@
 // import RIOT_CONSTANT from "@/constants/RIOT_CONSTANT";
 import { RotationChampionData } from "@/types/Championrotation";
+// const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || "";
 
 //CSR
 const fetchRotationChampion = async (): Promise<
   [number[], number[], number]
 > => {
-  // const res = await fetch(`${RIOT_CONSTANT.BASE_URL}/api/rotationChampions`);
   const res = await fetch(`/api/rotation`);
+  // const res = await fetch(`/api/rotation`);
   const data: RotationChampionData = await res.json();
   return [
     data.freeChampionIds,
