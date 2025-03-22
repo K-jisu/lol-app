422ec4f6925a12939a6972ffefc4f8040af55bc0
KJSoo95 <wltn7star@naver.com>
2025-03-22 21:42:37 +0900

    ♻️ refactor : routehandler => serverfetch

--- src/utils/riotApi.ts
+++ src/utils/riotApi.ts
@@ -1,11 +1,14 @@
 import RIOT_CONSTANT from "@/constants/RIOT_CONSTANT";
 import { RotationChampionData } from "@/types/Championrotation";
-
 //CSR
 const fetchRotationChampion = async (): Promise<
   [number[], number[], number]
 > => {
-  const res = await fetch(`${RIOT_CONSTANT.BASE_URL}/api/rotation`);
+  const res = await fetch(`${RIOT_CONSTANT.RIOT_ROTATION_CHAMPION_URL}`, {
+    headers: {
+      "X-Riot-Token": `${process.env.NEXT_PUBLIC_RIOT_API_KEY}` || "",
+    },
+  });
   const data: RotationChampionData = await res.json();
   return [
     data.freeChampionIds,
